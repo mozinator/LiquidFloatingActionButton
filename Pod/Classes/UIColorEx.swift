89c19634737abf37c7dff82506ca0fc1b5194d26
Fabian Aussems <fabianaussems@gmail.com>
2017-05-13 22:56:15 +0200

    uicolor fixes

--- Pod/Classes/UIColorEx.swift
+++ Pod/Classes/UIColorEx.swift
@@ -11,42 +11,42 @@ import UIKit
 
 extension UIColor {
     
-    var red: CGFloat {
+    var redV: CGFloat {
         get {
             let components = self.cgColor.components
             return components![0]
         }
     }
     
-    var green: CGFloat {
+    var greenV: CGFloat {
         get {
             let components = self.cgColor.components
             return components![1]
         }
     }
     
-    var blue: CGFloat {
+    var blueV: CGFloat {
         get {
             let components = self.cgColor.components
             return components![2]
         }
     }
     
-    var alpha: CGFloat {
+    var alphaV: CGFloat {
         get {
             return self.cgColor.alpha
         }
     }
 
     func alpha(_ alpha: CGFloat) -> UIColor {
-        return UIColor(red: self.red, green: self.green, blue: self.blue, alpha: alpha)
+        return UIColor(red: self.redV, green: self.greenV, blue: self.blueV, alpha: alpha)
     }
     
     func white(_ scale: CGFloat) -> UIColor {
         return UIColor(
-            red: self.red + (1.0 - self.red) * scale,
-            green: self.green + (1.0 - self.green) * scale,
-            blue: self.blue + (1.0 - self.blue) * scale,
+            red: self.redV + (1.0 - self.redV) * scale,
+            green: self.greenV + (1.0 - self.greenV) * scale,
+            blue: self.blueV + (1.0 - self.blueV) * scale,
             alpha: 1.0
         )
     }
